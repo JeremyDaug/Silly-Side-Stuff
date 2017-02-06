8421830f5961e87a40e64fd040e0e7ae5a1a1245
Jeremy Daugherty <Jeremy.Daug@gmail.com>
2017-02-06 13:43:29 -0600

    altering data to be nicer to program

--- Draconic Dict Prog/syls.py
+++ Draconic Dict Prog/syls.py
@@ -14,57 +14,89 @@ vowels = ['a', 'i', 'u', 'w', 'ai', 'ia', 'uw', 'wu']
 # for vowel in vowels:
 #     print('/'+vowel+'/')
 
+
 class data:
-    def __init__(self):
-        self.stuff = {}
-        self.stuff['syllables'] = [x+y for x in consonants for y in vowels]
-        self.stuff['syllables'].extend([y+x for x in consonants for y in vowels])
-        self.stuff['syllables'].extend(vowels)
-        self.stuff['taken'] = []
-        self.stuff['available'] = self.stuff['syllables']
-        self.stuff['dictionary'] = {}
+    def __init__(self, from_load):
+        self.syllables = []
+        self.taken = []
+        self.available = []
+        self.dictionary = dict()
+        self.tags = dict()
+        if from_load:
+            self.load()
+        else:
+            self.syllables = [x+y for x in consonants for y in vowels]
+            self.syllables.extend([y+x for x in consonants for y in vowels])
+            self.syllables.extend(vowels)
+            self.taken = []
+            self.available = [x for x in self.syllables]
+            self.dictionary = {}
         return
 
-    def out(self, string):
-        return '/'+string+'/'
+    def load(self):
+        temp = pickle.load(open('data.p', 'rb'))
+        self.syllables = temp['syllables']
+        self.taken = temp['taken']
+        self.available = temp['available']
+        self.dictionary = temp['dictionary']
+        self.tags = temp['tags']
+        return
+
+    def quit(self):
+        temp = dict()
+        temp['dictionary'] = self.dictionary
+        temp['syllables'] = self.syllables
+        temp['taken'] = self.taken
+        temp['available'] = self.available
+        temp['tags'] = self.tags
+        pickle.dump(temp, open('data.p', 'wb'))
+
+    def out(self, ret):
+        return '/'+ret+'/'
 
     def main(self):
         done = False
         while not done:
             txt = input('>>')
             if txt == 'show taken':
-                for i in self.stuff['taken']:
+                for i in self.taken:
                     print(self.out(i))
             elif txt == 'show available':
-                for i in self.stuff['available']:
+                for i in self.available:
                     print(self.out(i))
             elif txt == 'show syllables':
-                for i in self.stuff['syllables']:
+                for i in self.syllables:
                     print(self.out(i))
             elif txt == 'dictionary':
-                self.dictionary()
+                res = self.dictionary_menu()
+                if res == 'quit':
+                    done = True
             elif txt == 'random':
-                if not self.stuff['available']:
+                if not self.available:
                     print('All syllables taken.')
                 else:
-                    print(self.out(choice(self.stuff['available'])))
+                    print(self.out(choice(self.available)))
             elif txt == 'quit':
                 print('goodbye')
                 done = True
-            elif txt in self.stuff['available']:
+            elif txt == 'find word':
+                res = self.word_search()
+                if res == 'quit':
+                    done = True
+            elif txt in self.available:
                 prompt = 'What has taken it? \n' \
                          'To cancel input \'cancel\' or \'undo\'\n>>>'
                 info = input(prompt)
                 if info not in ['undo', 'cancel']:
                     self.update_taken(txt)
-                    self.stuff['dictionary'][self.out(txt)] = info
-            elif txt in self.stuff['taken']:
+                    self.dictionary[self.out(txt)] = info
+            elif txt in self.taken:
                 print('Already taken.')
                 print('%s : %s' % (self.out(txt), self.lookup_def(txt)))
             else:
                 self.output_help()
-
-        pickle.dump(self.stuff, open('data.p', 'wb'))
+        self.quit()
+        return
 
     def output_help(self):
         print('''Invalid input.
@@ -86,13 +118,70 @@ class data:
         self.stuff['available'].remove(txt)
 
     def dictionary(self):
-        print('WIP')
+        print('Entering Dictionary menu')
+        done = False
+        while not done:
+            txt = input('>>>')
+            if txt == 'show dictionary':
+                for i, j in self.stuff['dictionary'].items():
+                    print('%s : %s' % (i, j))
+            elif txt == 'quit':
+                return 'quit'
+            elif txt == 'back':
+                print('Returning to syllable menu.')
+                done = True
+            elif txt == 'find word':
+                self.word_search()
+            elif txt == 'add':
+                take = input('What would you like to add?')
+                if take not in ['cancel', 'undo']:
+                    split = take.split('-')
+                    invalid = self.check_syllables(split)
+                    collision = self.word_collision(take)
+                    if invalid:
+                        print('invalid Syllables:' + str(invalid))
+                        if '/' in take:
+                            print('Do not include slashes, only dashes.')
+                    elif self.out(take) in self.stuff['dictionary'].keys():
+                        p = '%s is taken. \n%s : %s' % (take,
+                                                        self.out(take),
+                                                        self.lookup_def(take))
+                        print(p)
+                    elif collision:
+                        print('%s had a collision with %s.' % (self.out(take),
+                                                               collision))
+                    else:
+                        self.add_def(take)
+            elif txt == 'change definition':
+                txt = input('What word?\n>>>')
+                if self.out(txt) in self.stuff['dictionary'].keys():
+                    n_def = input('What\'s the new definition\n>>>')
+                    self.stuff['dictionary'][self.out(txt)] = [n_def]
+            elif self.out(txt) in self.stuff['dictionary'].keys():
+                print('In dictionary.\n%s : %s' % (self.out(txt),
+                                                   self.lookup_def(txt)))
+        return
+
+    def add_def(self, word):
+        text = input('What does %s mean?\n>>>')
+        self.stuff['dictionary'][self.out(word)] = text
+
+    def word_collision(self, word):
+        return []
+
+    def check_syllables(self, check):
+        ret = []
+        for i in check:
+            if i not in self.stuff['syllables']:
+                ret.append(i)
+        return ret
+
+    def word_search(self):
         return
 
 
 if __name__ == '__main__':
     # load from pickle
     curr = data()
-    with open('data.p', 'rb') as stuff:
-        curr.stuff = pickle.load(stuff)
-        curr.main()
+    curr.load()
+    curr.main()
