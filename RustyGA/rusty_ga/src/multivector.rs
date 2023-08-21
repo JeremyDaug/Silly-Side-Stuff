f704e16edd86a75c57b9d3d2d85e09c4377aded6
JeremyDaugherty <Jeremy.Daug@gmail.com>
2023-08-21 15:05:41 -0500

    Inch more work, back to the salt mine

--- RustyGA/rusty_ga/src/multivector.rs
+++ RustyGA/rusty_ga/src/multivector.rs
@@ -1,11 +1,27 @@
-use crate::komponent::Komponent;
+use crate::{komponent::Komponent, orthonormalbases::OrthonormalBases};
 
 /// # Multivector
 /// 
 /// A Multivector is a collection of Komponents in a particular basis. 
+#[derive(Debug, Clone)]
 pub struct Multivector {
     /// The Components which make up this vector.
     pub komponents: Vec<Komponent>,
-    
-    grade: usize
+    /// The Basis Data available to the multivector.
+    onb: Rc<OrthonormalBases>,
+    /// The Magnitude, stored to make calculations easier.
+    magnitude: f64,
+}
+
+impl Multivector {
+    /// # New
+    /// 
+    /// Creates a new, empty, multivector.
+    pub fn new(onb: Rc<OrthonormalBases>) -> Multivector {
+        Multivector { 
+            komponents: vec![],
+            onb: onb.clone(),
+            magnitude: 0.0
+        }
+    }
 }
